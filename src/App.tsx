1a8a5cf7af9169752bf9052058ac7591e4f759ef
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2023-01-30 12:02:42 +0100

    Setup up loading, error and showing of posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
+import { useQuery, useMutation } from '@tanstack/react-query';
+
+const POSTS = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' }
+]
+
 function App() {
 
+  const postsQuery = useQuery({
+    // Unique ID for your query
+    queryKey: ['posts'],
+    // Function to be performed for these query keys - usually this will be a fetch or axios request
+    queryFn: () => wait(1000).then(() => [...POSTS])
+
+    // // Automatic re-tries multiple times. So you will notice that the errors take time to show because it tries a few times.
+    // queryFn: () => Promise.reject('Error Message')
+  })
+
+  if (postsQuery.isLoading) {
+    return (
+      <h1>Loading...</h1>
+    )
+  }
+
+  if (postsQuery.isError) {
+    return (
+      <pre>{JSON.stringify(postsQuery.error)}</pre>
+    )
+  }
+
   return (
-    <h1>Todo App</h1>
+    // Once the data is ready the .data attribute is populated and then the posts will show!
+    <h1>{postsQuery.data.map(posts => (
+      <div key={posts.id}>{posts.title}</div>
+    ))}</h1>
   )
+}
+
+function wait(duration: number) {
+  return new Promise(resolve => setTimeout(resolve, duration));
 }
 
 export default App
